c75c13d2c567986308e4e7aa62309029d38a5abf
prince-kn <pe64sahani@gmail.com>
2024-07-26 07:54:05 +0530

    Update cmds

---
+++ scripts/cmds/info.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const moment = require('moment-timezone');
+
+module.exports = {
+        config: {
+                name: "info",
+                version: "1.0",
+                author: "karan jalvanshi",
+                countDown: 20,
+                role: 0,
+                shortDescription: { vi: "", en: "" },
+                longDescription: { vi: "", en: "" },
+                category: "owner",
+                guide: { en: "" },
+                envConfig: {}
+        },
+        onStart: async function ({ message }) {
+                const botName = "Sarkari bf";
+                const botPrefix = "+";
+                const authorName = "KARAN JALVANSHI";
+                const ownAge = "20";
+                const teamName = "Github team";
+                const authorFB = "https://www.facebook.com/Legend.king.jalvanshi";
+                const authorInsta = "https://www.instagram.com/jalvanshi_king_2003";
+                const tikTok = "Tik tok to ban hai india mien 💀";
+                const urls = JSON.parse(fs.readFileSync('cliff.json'));
+                const link = urls[Math.floor(Math.random() * urls.length)];
+                const now = moment().tz('Asia/kolkata');
+                const date = now.format('MMMM Do YYYY');
+                const time = now.format('h:mm:ss A');
+                const uptime = process.uptime();
+                const seconds = Math.floor(uptime % 60);
+                const minutes = Math.floor((uptime / 60) % 60);
+                const hours = Math.floor((uptime / (60 * 60)) % 24);
+                const days = Math.floor(uptime / (60 * 60 * 24));
+                const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
+                message.reply({
+                        body: `《  Bot & Owner Info 》
+\Name: ${botName}
+\Bot Prefix: ${botPrefix}
+\owner: ${authorName}
+\age : ${ownAge}
+\Facebook: ${authorFB}
+\Instagram: ${authorInsta}
+\TikTok: ${tikTok}
+\Datee: ${date}
+\Time: ${time}
+\Team: ${teamName}
+\Uptime: ${uptimeString}
+\===============`,
+                        attachment: await global.utils.getStreamFromURL(link)
+                });
+        },
+        onChat: async function ({ event, message, getLang }) {
+                if (event.body && event.body.toLowerCase() === "info") {
+                        this.onStart({ message });
+                }
+        }
+};
