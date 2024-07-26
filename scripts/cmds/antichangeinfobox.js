7df3c8c63571f6b9a95c8ab47ced1ee3d88490e2
prince-kn <pe64sahani@gmail.com>
2024-07-26 07:36:34 +0530

    Update antichangeinfobox.js

--- scripts/cmds/antichangeinfobox.js
+++ scripts/cmds/antichangeinfobox.js
@@ -2,7 +2,7 @@ const { getStreamFromURL, uploadImgbb } = global.utils;
 
 module.exports = {
 	config: {
-		name: "antichangeinfobox",
+		name: "bachha",
 		version: "1.9",
 		author: "NTKhang",
 		countDown: 5,
