63f39305462cbe340585bb79ada34fbe4b974e9e
prince-kn <pe64sahani@gmail.com>
2024-07-28 08:26:49 +0530

    Update cmds

---
+++ scripts/cmds/kick1.js
@@ -0,0 +1,50 @@
+module.exports = {
+ config: {
+ name: "kick1",
+ version: "1.0",
+ author: "KARAN JALVANSHI",
+ countDown: 5,
+ role: 2,
+ shortDescription: "no prefix",
+ longDescription: "no prefix",
+ category: "no prefix",
+ },
+ 
+ onStart: async function(){}, 
+ onChat: async function({ message, event, args, threadsData, api, getLang }) {
+   const input = event.body;
+ if (input && input.trim().toLowerCase().startsWith('kick1') || input && input.trim().toLowerCase().startsWith('kick2')){
+    const data = input.split(" ");
+    data.shift();
+   const prompt = data.join(" ");
+   
+   const adminIDs = await threadsData.get(event.threadID, "adminIDs");
+   if (!adminIDs.includes(api.getCurrentUserID()))
+     return message.reply(getLang("needAdmin"));
+   async function kickAndCheckError(uid) {
+     try {
+       await api.removeUserFromGroup(uid, event.threadID);
+     }
+     catch (e) {
+       message.reply(getLang("needAdmin"));
+       return "ERROR";
+     }
+   }
+   if (!prompt[0]) {
+     if (!event.messageReply)
+       return message.SyntaxError();
+     await kickAndCheckError(event.messageReply.senderID);
+   }
+   else {
+     const uids = Object.keys(event.mentions);
+     if (uids.length === 0)
+       return message.SyntaxError();
+     if (await kickAndCheckError(uids.shift()) === "ERROR")
+       return;
+     for (const uid of uids)
+       api.removeUserFromGroup(uid, event.threadID);
+
+ }
+ }
+}
+};
