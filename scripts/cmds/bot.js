45af6579acbd6d26f73970cfa88f975cf4bdef89
prince-kn <pe64sahani@gmail.com>
2024-08-09 12:48:19 +0530

    Update cmds

---
+++ scripts/cmds/bot.js
@@ -0,0 +1,57 @@
+,cmd install ai.js const axios = require('axios');
+const UPoLPrefix = [
+  'bot',
+  'BOT',
+  'Bbot',
+  'bbot',
+  'Robot'
+]; 
+
+  module.exports = {
+  config: {
+    name: 'Bot',
+    version: '1.2.1',
+    role: 0,
+    category: 'AI',
+    author: 'Karan jalvanshi',
+    shortDescription: '',
+    longDescription: '',
+  },
+  
+  onStart: async function () {},
+  onChat: async function ({ message, event, args, api, threadID, messageID }) {
+      
+      const ahprefix = UPoLPrefix.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+      if (!ahprefix) {
+        return; 
+      } 
+      
+     const upol = event.body.substring(ahprefix.length).trim();
+   if (!upol) {
+        await message.reply('Are age bhi bolo baby');
+        return;
+      }
+      
+      const apply = ['Awww🥹, maybe you need my help', 'How can i help you?', 'How can i assist you today?', 'How can i help you?🙂'];
+      
+     const randomapply = apply[Math.floor(Math.random() * apply.length)];
+
+     
+      if (args[0] === 'hi') {
+          message.reply(`${randomapply}`);
+          return;
+      }
+      
+    const encodedPrompt = encodeURIComponent(args.join(" "));
+
+   await message.reply('');
+  
+    const response = await axios.get(`https://sandipbaruwal.onrender.com/gemini?prompt=${encodedPrompt}`);
+ 
+     const UPoL = response.data.answer; 
+
+      const upolres = `${UPoL}`;
+      
+        message.reply(upolres);
+  }
+};
