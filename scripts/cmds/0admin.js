8b567d1d79285197c29a7e18ddc4fe97ffd24601
prince-kn <pe64sahani@gmail.com>
2024-08-14 20:23:40 +0530

    Update 0admin.js

--- scripts/cmds/0admin.js
+++ scripts/cmds/0admin.js
@@ -1,21 +1,22 @@
 module.exports = {
-        config: {
-                        name: "admin",
-                        version: "1.0",
-                        author: "karan jalvanshi",
-                        countDown: 5,
-                        role: 0,
-                        shortDescription: "sarcasm",
-                        longDescription: "sarcasm",
-                        category: "reply",
-        },
-onStart: async function(){}, 
-onChat: async function({
-        event,
-        message,
-        getLang
-}) {
-        if (event.body && event.body.toLowerCase() == "admin") return message.reply("Admin :-  karan jalvanshi
-Owner Fb id :- https://www.facebook.com/Legend.king.jalvanshi");
+ config: {
+	 name: "Owner",
+	 version: "1.0",
+	 author: "karan jalvanshi",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "owner") {
+ return message.reply({
+ body: "𝐎𝐖𝐍𝐄𝐑 𝐍𝐀𝐌𝐄 :- 𝐊𝐀𝐑𝐀𝐍 𝐉𝐀𝐋𝐕𝐀𝐍𝐒𝐇𝐈\n\n𝐅𝐁 𝐈𝐃 :- https://www.facebook.com/Legend.king.jalvanshi",
+ attachment: await global.utils.getStreamFromURL("http://g-v1.onrender.com/1hOXgYjqa.jpg")
+ });
+ }
+ }
 }
-};
